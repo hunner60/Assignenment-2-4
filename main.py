9d84414abe12f135ee6032a58d5043096ed7c181
hunner60 <hnelson13@hawkmail.hccfl.edu>
2025-03-02 14:01:43 +0000

    Restored to '76d422ca495d8dfeddc3196c054183f6497446fe'
    Replit-Restored-To: 76d422ca495d8dfeddc3196c054183f6497446fe

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-
 # input statements
 salary = float(input())
 numDependents = float(input())
 
-# tax calculations
-state_tax = salary * 0.065  # State tax rate of 6.5%
-federal_tax = salary * 0.28  # Federal tax rate of 28%
-dependent_deduction = numDependents * 1500.0  # Deduction per dependent
-
-# calculate total withholding and take home pay
-total_withholding = state_tax + federal_tax + dependent_deduction
-takeHomePay = salary - total_withholding
+# calculate taxes here
 
 # output statements
-print("State Tax: $" + str(state_tax))
-print("Federal Tax: $" + str(federal_tax))
-print("Dependents: $" + str(dependent_deduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
